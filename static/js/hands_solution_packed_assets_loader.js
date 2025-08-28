fbdbcd9e3d888f0dd9f685b6ea1d418309c7e3ed
Wink Doubleguns <winkdoubleguns@gmail.com>
2025-08-27 20:19:08 -0500

    commented out line that was originally in place for pulling remote modules

--- static/js/hands_solution_packed_assets_loader.js
+++ static/js/hands_solution_packed_assets_loader.js
@@ -19,12 +19,15 @@
         // web worker
         PACKAGE_PATH = encodeURIComponent(location.pathname.toString().substring(0, location.pathname.toString().lastIndexOf('/')) + '/');
       }
+
       var PACKAGE_NAME = 'blaze-out/k8-opt/genfiles/third_party/mediapipe/web/solutions/hands/hands_solution_packed_assets.data';
       var REMOTE_PACKAGE_BASE = 'hands_solution_packed_assets.data';
       if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
         Module['locateFile'] = Module['locateFilePackage'];
         err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
       }
+
+      // var REMOTE_PACKAGE_NAME = Module['locateFile'] ? Module['locateFile'](REMOTE_PACKAGE_BASE, '') : REMOTE_PACKAGE_BASE;
       // Force loading from the local /static/ path where the .data file is served
       var REMOTE_PACKAGE_NAME = (function() {
         try {
